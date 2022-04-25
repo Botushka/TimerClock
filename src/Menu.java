d9eb53e6ac4b63839f19707ccecfc84cb924f956
Stevve <91798274+Botushka@users.noreply.github.com>
2022-04-25 14:30:01 +0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -14,26 +14,26 @@ public class Menu extends LiveClock {
         System.out.println("Press 1 for LiveClock");
         System.out.println("Press 2 for Timer");
         System.out.println("Press 3 to quit");
-        Scanner orange = new Scanner(System.in);
-        int k = Integer.valueOf(orange.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.valueOf(scanner.nextLine());
         
         while(true) {
 
-            if (k == 1) {
+            if (num == 1) {
                 if (true) {
                     LiveClock LiveClock = new LiveClock();
                     System.out.println(LiveClock);
 
                 }
             }
-            if (k == 2) {
+            if (num == 2) {
                 if (true) {
                     Timer Timer = new Timer();
                     System.out.println(Timer);
                 }
             }
 
-            if (k == 3) {
+            if (numk == 3) {
                 break;
 
             }
